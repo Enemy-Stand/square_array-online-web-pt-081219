d46e00b780a8c0f7e1412d77841c37f6f4c234aa
Joseph Sesti <sestijoey63@gmail.com>
2019-08-25 20:58:36 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,5 @@
 def square_array(array)
+<<<<<<< HEAD
   numbers = []
   array.each { |i| numbers << i ** 2}
   numbers
@@ -6,3 +7,11 @@ end
 
 numbers_to_square = [1,2]
 p square_array(numbers_to_square)
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
+end
+
+my_arr = [1, 2]
+p square_array(my_arr)
+>>>>>>> d0753e1013552eff871e42006f76f5bb4c09210a
